787270e348218e06939381760caee47bdb381801
Eric Fornstedt <eric.fornstedt@ruag.com>
2020-10-06 14:51:14 +0200

    Add message functionality

--- timemeasure.c
+++ timemeasure.c
@@ -3,7 +3,7 @@
 #include "timemeasure.h"
 
 #ifndef DISABLE_MEASUREMENT
-#define MAX_NESTED 128
+#define MAX_NESTED 32
 #define CPU_LIMIT  0.000
 #define REAL_LIMIT 0.000
 
@@ -11,7 +11,12 @@ static unsigned int _nestedLevel = 0;
 static struct timespec _cpuStart[MAX_NESTED];
 static struct timespec _realStart[MAX_NESTED];
 
-void tic()
+void _tic(char const *fileName, char const *callerName)
+{
+    _tic_msg(fileName, callerName, NULL);
+}
+
+void _tic_msg(char const *fileName, char const *callerName, const char *msg)
 {
     if (_nestedLevel < MAX_NESTED)
     {
@@ -22,10 +27,21 @@ void tic()
     {
         printf("tic: Maximum nested depth reached.\n");
     }
+
     _nestedLevel++;
+
+    if (msg != NULL)
+    {
+        printf("%s:%s(), level %d, start - %s\n", fileName, callerName, _nestedLevel, msg);
+    }
 }
 
 void _toc(char const *fileName, char const *callerName)
+{
+    _toc_msg(fileName, callerName, NULL);
+}
+
+void _toc_msg(char const *fileName, char const *callerName, const char *msg)
 {
     if (_nestedLevel < MAX_NESTED +1 && _nestedLevel > 0)
     {
@@ -41,7 +57,14 @@ void _toc(char const *fileName, char const *callerName)
 
         if (elapsedCpu > CPU_LIMIT || elapsedReal > REAL_LIMIT)
         {
-            printf("%s:%s(), %d level, %.3f ms (real), %.3f ms (cpu).\n", fileName, callerName, _nestedLevel, elapsedReal, elapsedCpu);
+            if (msg != NULL)
+            {
+                printf("%s:%s(), level %d, %.3f ms (real), %.3f ms (cpu) - %s\n", fileName, callerName, _nestedLevel, elapsedReal, elapsedCpu, msg);
+            }
+            else
+            {
+                printf("%s:%s(), level %d, %.3f ms (real), %.3f ms (cpu)\n", fileName, callerName, _nestedLevel, elapsedReal, elapsedCpu);
+            }
         }
     }
     else
--- timemeasure.h
+++ timemeasure.h
@@ -2,13 +2,22 @@
 // #define DISABLE_MEASUREMENT
 
 #ifdef DISABLE_MEASUREMENT
-  #define tic()
-  #define toc()
-#else
-  #define toc() _toc(__FILE__, __func__)
-#endif
 
-#ifndef DISABLE_MEASUREMENT
-void tic();
+#define tic()
+#define tic_msg(message)
+#define toc()
+#define toc_msg(message)
+
+#else
+
+#define tic() _tic(__FILE__, __func__)
+#define tic_msg(message) _tic_msg(__FILE__, __func__, message)
+#define toc() _toc(__FILE__, __func__)
+#define toc_msg(message) _toc_msg(__FILE__, __func__, message)
+
+void _tic(char const *fileName, char const *callerName);
 void _toc(char const *fileName, char const *callerName);
+void _toc_msg(char const *fileName, char const *callerName, const char *msg);
+void _tic_msg(char const *fileName, char const *callerName, const char *msg);
+
 #endif
